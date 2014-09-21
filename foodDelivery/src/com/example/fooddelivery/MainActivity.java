eb804fb7c615d87d532b315fcf601c33082c1ae4
weidaw <ouitatuwang@gmail.com>
2014-09-20 21:45:05 -0400

    lesson 1

--- foodDelivery/src/com/example/fooddelivery/MainActivity.java
+++ foodDelivery/src/com/example/fooddelivery/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.fooddelivery;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	public final static String EXTRA_MESSAGE = "com.excample.fooddelivery.MESSAGE";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,20 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public void sendMessage(View view){
+    	Intent intent =  new Intent(this, DisplayMessageActivity.class);
+    	
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	
+    	String message = editText.getText().toString();
+    	
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	
+    	startActivity(intent);
+    	
+    	
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -33,4 +52,5 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
 }
